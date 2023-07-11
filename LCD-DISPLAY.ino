b42b51c0daed2046c9f9cdeab7afbc91c4ead19c
Alex <alexvatti@gmail.com>
2023-07-11 08:25:55 +0530

    Create LCD-DISPLAY.ino

---
+++ LCD-DISPLAY.ino
@@ -0,0 +1,27 @@
+//16x2 LCD INTERFACE WITH ARDUINO UNO BOARD
+// It is connected Over I2C Interface
+// SDA -  A4
+// SCL -  A5
+// VCC -  5V
+// GND -  ARDUINO UNO GND
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("ARDUINO PROJECTS");
+  lcd.setCursor(0,1);
+  lcd.print("PLS  SUBSCRIBE");
+}
+
+void loop()
+{
+
+}
