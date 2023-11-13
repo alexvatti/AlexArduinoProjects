10ca35a0c3fd22a61340b2c9238a0e1eaaf364e2
Alex <alexvatti@gmail.com>
2023-11-13 08:45:48 +0530

    Update servo-motor-example.py

--- MicroPython/esp32-examples/servo-motor-example.py
+++ MicroPython/esp32-examples/servo-motor-example.py
@@ -1,1 +1,18 @@
+'''
+ servo motor from the ESP32 with a Python script
+ '''
+from machine import Pin, PWM
+from time import sleep
 
+frequency = 50
+servo = PWM(Pin(13), frequency)
+
+while True:
+  #servo motor angle 0 to 180
+  for duty_cycle in range(0, 123,10):
+    servo.duty(duty_cycle)
+    sleep(1)
+    
+  for duty_cycle in range(123, -1, -10):
+    servo.duty(duty_cycle)
+    sleep(1)
